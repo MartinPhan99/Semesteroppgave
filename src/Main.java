8261e20d42c13258139fc8915d3be1af00d91fbc
tm2_93@hotmail.com <Toddy2727>
2019-03-27 15:15:50 +0100

    Commit to person branche

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("hello people");
         System.out.println("Hello agian");
         System.out.println("hello again nr 2");
         System.out.println("hello again nr 3");
+        System.out.println("My branch");
 
     }
 }
