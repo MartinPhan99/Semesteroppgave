4db080b38b1e11dc3f28ce8552bc7d200d4d7d36
Martin Phan <Martin_phan99@hotmail.com>
2019-04-01 14:59:05 +0200

    Dette er Martins forsøk La til Kontakperson.class og fxml

---
+++ src/sample/controllerSide3.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class controllerSide3 {
+
+
+}
